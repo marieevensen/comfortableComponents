30e7df171da950ef138417fc6243bc3eca303a8d
SigurdHaugbro <sigurdhaugbro@icloud.com>
2021-11-29 23:31:23 +0100

    hei funka dette?

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import Table from './views/Table.vue';
 import Quiz from './views/Quiz.vue';
 
 export default [
-    { name: 'todo',     path: '/todo',      component: Todo },
-    { name: 'table',    path: '/table',     component: Table},
-    { name: 'quiz',     path: '/quiz',      component: Quiz}
+    { name: 'todo', path: '/todo', component: Todo },
+    { name: 'table', path: '/table', component: Table },
+    { name: 'quiz', path: '/quiz', component: Quiz }
 ]
+
+// dette er en test komentar :)
