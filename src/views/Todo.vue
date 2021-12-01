59e132cd09f7a68a891d590db46372660b68a341
marieevensen <marieb.evensen@hotmail.com>
2021-12-01 17:40:00 +0100

    hei jeg er dæv

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,26 +2,18 @@
   <div class="todo">
     <div class="todo__title">To do today</div>
     <div class="todo__text">
-      <input
-        class="todo__text-write"
-        type="text"
-        placeholder="Add your task here"
-        v-model="task"
-      />
-      <button class="todo__text-submit" @click="submitForm">ss</button><br />
-      <div class="todo__text-task" v-for="(task, index) in tasks" :value="task">
-        {{ task.name }}
-        <input
-          class="todo__text-box"
-          @click="finishedTask(index)"
-          type="checkbox"
-        />
+      <input class="todo__text-write" type="text" placeholder="Add your task here" v-model="task"/>
+      <button class="todo__text-submit" @click="addTask">ss</button><br />
+      <div class="todo__text-task" v-for="task in pendingTasks" :value="task">
+        <button class="todo__text--btn" @click="deleteTask(index)"><img src="assets/trash.png " alt=""></button>
+        {{ task.text }}
+        <input class="todo__text-box" @click="checkTask" type="checkbox"/>
       </div>
     </div>
     <div class="todo__finished">
       Finished Tasks:
-      <div class="todo__finished-task" v-for="(task, index) in tasks" :value="task">
-        {{ task.finished }}
+      <div class="todo__finished-task" v-for="task in doneTasks" :value="task">
+        {{ task.checked }}
       </div>
     </div>
   </div>
@@ -36,76 +28,91 @@ export default {
     };
   },
 
+  computed: {
+    pendingTasks() {
+      return this.tasks.filter(task => task.done === false)
+    },
+    doneTasks() {
+      return this.tasks.filter(task => task.done === true)
+    }
+  },
+
   methods: {
-    submitForm() {
+    addTask() {
       if (this.task.length === 0) return;
 
       this.tasks.push({
-        name: this.task,
+        text: this.task,
+        done: false
       });
     },
-    finishedTask(index) {
+    deleteTask(index) {
+       this.tasks.splice(index, 1);
+    },
+    checkTask() {
       this.tasks.push({
-        finished: this.task,
-      });
-    },
-    // deleteTask(index) {
-    //   this.tasks.splice(index, 1);
-    // },
-  },
+        checked: this.task,
+        done: true
+      })
+    }
+  }
 };
 </script>
 
 <style>
-.todo {
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  margin-top: 30px;
-}
+  .todo {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    margin-top: 30px;
+  }
 
-.todo__title {
-  margin: 20px 0px;
-  margin-left: 60px;
-}
+  .todo__title {
+    margin: 20px 0px;
+    margin-left: 60px;
+  }
 
-.todo__text {
-  border-right: 2px black solid;
-}
+  .todo__text {
+    border-right: 2px black solid;
+  }
 
-.todo__text-write {
-  margin: 20px 6px 30px 20px;
-  border: none;
-  padding-left: 4px;
-}
+  .todo__text-write {
+    margin: 20px 6px 30px 20px;
+    border: none;
+    padding-left: 4px;
+  }
 
-.todo__text-submit {
-  background-color: grey;
-  border-radius: 10px;
-  color: grey;
-  margin-right: 50px;
-}
+  .todo__text-submit {
+    background-color: grey;
+    border-radius: 10px;
+    color: grey;
+    margin-right: 50px;
+  }
 
-.todo__text-submit:hover {
-  color: black;
-  background-color: black;
-}
+  .todo__text-submit:hover {
+    color: black;
+    background-color: black;
+  }
 
-.todo__text-task {
-  margin: 0px 20px;
-  padding-left: 4px;
-  font-size: 0.8em;
-}
+  .todo__text-task {
+    margin: 0px 20px;
+    padding-left: 4px;
+    font-size: 0.8em;
+  }
 
-.todo__text-box:focus {
-  background-color: black;
-}
+  .todo__text--btn img {
+    width: 30px;
+  }
 
-.todo__finished {
-  margin: 20px 0px 20px 50px;
-}
+  .todo__text-box:focus {
+    background-color: black;
+  }
 
-.todo__finished-task:first-child {
-  margin: 30px 0px 0px 0px;
-}
+  .todo__finished {
+    margin: 20px 0px 20px 50px;
+  }
+
+  .todo__finished-task:first-child {
+    margin: 30px 0px 0px 0px;
+  }
 </style>
