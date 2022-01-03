43550dbf3201c8970cba9951ff1637628b002722
sigurdhaugbro <79162508+sigurdhaugbro@users.noreply.github.com>
2022-01-03 13:16:30 +0100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,17 +8,23 @@ import Home from './views/Home.vue';
 export default [
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     { name: 'home', path: '/', component: Home },
 =======
     { name: 'home', path: '/home', component: Home },
 >>>>>>> parent of 05dfeaf (template finished)
 
 =======
+=======
+>>>>>>> parent of 3353aae (Merge branch 'master' of https://github.com/marieevensen/oppgave-tema-4)
     { name: 'todo', path: '/todo', component: Todo },
     { name: 'table', path: '/table', component: Table },
     { name: 'quiz', path: '/quiz', component: Quiz },
     { name: 'form', path: '/form', component: Form },
     { name: 'slideshow', path: '/slideshow', component: Slideshow },
     { name: 'home', path: '/', component: Home}
+<<<<<<< HEAD
 >>>>>>> parent of 2c8a8f1 (creating template)
+=======
+>>>>>>> parent of 3353aae (Merge branch 'master' of https://github.com/marieevensen/oppgave-tema-4)
 ]
